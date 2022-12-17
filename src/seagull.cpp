3c83f0636f5e62f72a9856cf9ffd477fd92f4d04
Jett Thompson <jett.thompson.59@gmail.com>
2022-12-17 20:41:53 +1100

    begin to add the implementation as well

---
+++ src/seagull.cpp
@@ -0,0 +1,1 @@
+#include <seagull_internal.h>
---
+++ src/seagull_internal.h
@@ -0,0 +1,13 @@
+#ifndef SEAGULL_SEAGULL_INTERNAL_H
+#define SEAGULL_SEAGULL_INTERNAL_H
+
+#include <GLFW/glfw3.h>
+#include <seagull/seagull.h>
+
+namespace seagull {
+struct GameContext {
+  GLFWwindow *window;
+};
+} // namespace seagull
+
+#endif
